0c05372fff16899ae80a0928289365aef3cefeb8
Nisha Bhimani <44516867+NishaBhimani@users.noreply.github.com>
2023-10-21 22:46:35 +0530

    Update category.component.ts

--- Code/go-store/src/app/pages/category/category.component.ts
+++ Code/go-store/src/app/pages/category/category.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductService } from 'services/product.service';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.scss']
+  styleUrls: ['./category.component.scss'],
 })
 export class CategoryComponent {
+  categories: Array<string> = [];
 
+  columns: string[] = ['name', 'action'];
+
+  constructor(private productService: ProductService, private router: Router) {}
+  ngOnInit(): void {
+    this.productService.getCategories().subscribe((response) => {
+      this.categories = response;
+    });
+  }
+
+  getCategoryProducts = (category: string) => {
+    this.productService.getCategoryProducts(category).subscribe((response) => {
+      console.log(response);
+    });
+  };
 }
