ef6da54eff62538d8beb0dd2816db4673997f737
PinalBhimani <pinal.bhimani@tatvasoft.com>
2023-10-22 22:27:34 +0530

    Router link added

--- Code/go-store/src/app/layouts/sidebar/sidebar.component.ts
+++ Code/go-store/src/app/layouts/sidebar/sidebar.component.ts
@@ -1,13 +1,13 @@
 import { MatListModule } from '@angular/material/list';
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   standalone: true,
-  imports: [MatIconModule, MatListModule],
+  imports: [RouterModule, MatIconModule, MatListModule],
 })
 export class SidebarComponent {
   constructor() {}
