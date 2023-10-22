688a14b565fbe4f90cee3d05b7fa26a22a31b63a
PinalBhimani <pinal.bhimani@tatvasoft.com>
2023-10-22 22:27:04 +0530

    Interceptor - added

--- Code/go-store/src/app/app.module.ts
+++ Code/go-store/src/app/app.module.ts
@@ -1,13 +1,19 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MaterialModule } from 'app/modules/material/material.module';
 import { AppRoutingModule } from './modules/routing/app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { APP_SERIVCE_CONFIG, APP_CONFIG } from 'utils/appconfig.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ComponentsModule } from './modules/component/component.module';
 import { CommonModule } from '@angular/common';
+import { RequestInterceptor } from './../services/request.interceptor';
+import { ResponseInterceptor } from './../services/response.interceptor';
+import { InitService } from './../services/init.service';
+function initFactory(initService: InitService) {
+  return () => initService.init();
+}
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -19,7 +25,17 @@ import { CommonModule } from '@angular/common';
     AppRoutingModule,
     ComponentsModule,
   ],
-  providers: [{ provide: APP_SERIVCE_CONFIG, useValue: APP_CONFIG }],
+  providers: [
+    { provide: APP_SERIVCE_CONFIG, useValue: APP_CONFIG },
+    { provide: HTTP_INTERCEPTORS, useClass: RequestInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ResponseInterceptor, multi: true },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initFactory,
+      deps: [InitService],
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ Code/go-store/src/services/init.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InitService {
+  config: any;
+  constructor(private httpClient: HttpClient) {}
+  init() {
+    return this.httpClient
+      .get('./../../assets/config.json')
+      .pipe(tap((config) => (this.config = config)));
+  }
+}
